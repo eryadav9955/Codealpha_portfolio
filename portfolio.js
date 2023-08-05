d037809ee7795ed5182ceeea09672e6e737b0eab
RAUSHAN KUMAR <119736641+eryadav9955@users.noreply.github.com>
2023-08-06 00:52:58 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new typed(".text", {
+    Strings: { "Frontend Developer", "Youtouber", "web developer" },
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 5000,
+    loop: true
+})
